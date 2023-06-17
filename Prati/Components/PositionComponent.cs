0c9bca314f4665ed7227c81929385312d787a145
Lorenzo Prati <lorenzo.prati3@studio.unibo.it>
2023-06-17 20:12:58 +0200

    add solution

---
+++ Prati/Components/PositionComponent.cs
@@ -0,0 +1,17 @@
+﻿namespace Components
+{
+    public class PositionComponent
+    {
+        private int _x;
+        private int _y;
+
+        public PositionComponent(int x, int y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        public int GetX => _x;
+        public int GetY => _y;
+    }
+}
