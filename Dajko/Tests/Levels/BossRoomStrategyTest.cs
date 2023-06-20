601c7a436b02cdd8b6bd3a107593dede9b991f8a
Emanuele Dajko <ema.unibo@gmail.com>
2023-06-20 15:56:52 +0200

    Boss room strategy test.

---
+++ Dajko/Tests/Levels/BossRoomStrategyTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace Tests.Levels
+{
+    [TestFixture]
+    public class BossRoomStartegyTests
+    {
+        [Test]
+        public void Generate_ReturnsListOfEntities()
+        {
+            // Arrange
+            BossRoomStartegy strategy = new BossRoomStartegy();
+            // Act
+            List<Entity> result = strategy.Generate();
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Count > 0);
+
+        }
+    }
+}
