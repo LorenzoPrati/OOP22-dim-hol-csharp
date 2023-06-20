bd139a93c4a5438def2360b8989bd0fe5b0f7ded
Emanuele Dajko <ema.unibo@gmail.com>
2023-06-20 12:31:03 +0200

    Adds the interface of the level manager.

---
+++ Dajko/Levels/ILevelManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Dimhol.Levels
+{
+    /// Generates a new level for the game, including the placement of the player and enemies.
+    public interface ILevelManager
+    {
+        /// Changes the current level in the game and returns the updated list of entities.
+        List<Entity> ChangeLevel(List<Entity> entities);
+
+        /// Retrieves the tile map for the current level.
+        TileMap GetTileMap();
+    }
+}
