e969a6150d2a66d0ea3496601296177b1385b5a5
Alessandra Versari <alessandra.versari@studio.unibo.it>
2023-06-20 11:54:34 +0200

    Add componentTest for healthComponent and coinPocketComponent.

---
+++ Versari/Test/ComponentTest.cs
@@ -0,0 +1,32 @@
+﻿using Components;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test;
+public class ComponentTest
+{
+    private static int MaxHealth = 10;
+    private static int NewMaxHealth = 20;
+    private static int NewCurrentHealth = 8;
+    private static int InitialCoinAmount = 30;
+    private static int NewCoinAmount = 25;
+    [TestMethod]
+    public void HealthComponentTest()
+    {
+        var healthComp = new HealthComponent(MaxHealth);
+        Assert.AreEqual(healthComp.CurrentHealth, MaxHealth);
+        Assert.AreEqual(healthComp.MaxHealth, MaxHealth);
+        healthComp.MaxHealth = NewMaxHealth;
+        Assert.AreEqual(healthComp.MaxHealth, NewMaxHealth);
+        healthComp.CurrentHealth = NewCurrentHealth;
+        Assert.AreEqual(healthComp.CurrentHealth, NewCurrentHealth);
+    }
+
+    [TestMethod]
+    public void CoinPocketComponentTest()
+    {
+        var coinComp = new CoinPocketComponent(InitialCoinAmount);
+        Assert.AreEqual(coinComp.Coins, InitialCoinAmount);
+        coinComp.Coins = NewCoinAmount;
+        Assert.AreEqual(coinComp.Coins, NewCoinAmount);
+    }
+}
