d212d042014fa41d8f592205ae1a4d7cd505cc7f
Emanuele Dajko <ema.unibo@gmail.com>
2023-06-21 12:15:39 +0200

    Introduces the exception used for the Map loader implementation.

---
+++ Dajko/Map/MapLoadingException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Map
+{
+    /// <summary>
+    /// Exception that is thrown when an error occurs during map loading.
+    /// It is a subclass of the Exception class, indicating that it is a checked exception.
+    /// </summary>
+    public class MapLoadingException : Exception
+    {
+        public MapLoadingException(string message) : base(message)
+        {
+        }
+
+        public MapLoadingException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
