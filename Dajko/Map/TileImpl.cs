6df73ecc908b4a73e4239265df2bad5206288851
Emanuele Dajko <ema.unibo@gmail.com>
2023-06-21 12:16:59 +0200

    Updates the class.

---
+++ Dajko/Map/TileImpl.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Map
+{
+    /// <summary>
+    /// A Tile represents a single tile in a game map, with information about whether it is collidable.
+    /// </summary>
+    public class TileImpl : ITile
+    {
+        private readonly bool walkable;
+        private readonly int tileSetId;
+
+        /// <summary>
+        /// Constructs a new Tile with the specified collidability.
+        /// </summary>
+        public TileImpl(int tileSetId, bool walkable)
+        {
+            this.tileSetId = tileSetId;
+            this.walkable = walkable;
+        }
+
+        /// <summary>
+        /// Returns whether this Tile is collidable.
+        /// </summary>
+        public bool IsWalkableTile()
+        {
+            return walkable;
+        }
+
+        /// <summary>
+        /// Returns the tile set ID.
+        /// </summary>
+        public int GetTileSetId()
+        {
+            return tileSetId;
+        }
+    }
+}
