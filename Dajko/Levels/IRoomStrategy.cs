79a0ee5d12179cfa7af8297a6d8b5b631e8c8f60
Emanuele Dajko <ema.unibo@gmail.com>
2023-06-21 12:12:52 +0200

    Adds the Room strategy interface.

---
+++ Dajko/Levels/IRoomStrategy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Entity;
+using Components;
+
+namespace Levels
+{
+    
+    /// <summary>
+    /// The RoomStrategy interface is used to define the behavior of each different game room.
+    /// Implementations of this interface provide methods to generate entities for the game room, such as enemies and the player
+    /// and position them within the room.
+    /// </summary>
+
+    public interface IRoomStrategy
+    {
+        /// <summary>
+        /// Generate entities for the game room.
+        /// This method should be implemented to create the enemies, the player,
+        /// and position them into the room according to the specific strategy of the room.
+        /// </summary>
+        List<IEntity> Generate(IEntity? entity, HashSet<Tuple<int, int>> availableTiles, List<IEntity> entities);
+    }
+}
