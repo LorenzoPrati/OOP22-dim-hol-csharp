ef1bfc754f3ce32f36134a3a8dcdbdf797b835ae
Emanuele Dajko <ema.unibo@gmail.com>
2023-06-21 12:13:02 +0200

    Adds the Tile interface.

---
+++ Dajko/Map/ITile.cs
@@ -0,0 +1,20 @@
+﻿namespace Map
+{
+    /// <summary>
+    /// Represents a tile in the game map.
+    /// Tiles are used to build the game world and provide information
+    /// about their properties such as walkability and tile set ID.
+    /// </summary>
+    public interface ITile
+    {
+        /// <summary>
+        /// Returns whether this Tile is walkable.
+        /// </summary>
+        bool IsWalkableTile();
+
+        /// <summary>
+        /// Returns the tile set ID.
+        /// </summary>
+        int GetTileSetId();
+    }
+}
